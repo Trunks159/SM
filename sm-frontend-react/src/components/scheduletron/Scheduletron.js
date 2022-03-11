6b445ee186ed57adf1b32c68be5fba5e64d5a8f1
Trunks159 <49618679+Trunks159@users.noreply.github.com>
2022-03-11 13:29:27 -0500

    Im not sure

--- sm-frontend-react/src/components/scheduletron/Scheduletron.js
+++ sm-frontend-react/src/components/scheduletron/Scheduletron.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/styles';
+import HealthBar from './HealthBar';
 
 const styles = ()=>({
     main : {
@@ -11,11 +12,54 @@ class  Scheduletron extends Component {
     state = {  } 
     render() { 
         const {classes} = this.props;
-        const schedules = 
-        return (
-            <div>
-                <p>SELECT SCHEDULE</p>
+        const schedules = [[
+            {
+                weekday : 'Monday',
+                date : '9/17',
+                health : '.9',
+            },
+            {
+                weekday : 'Monday',
+                date : '9/18',
+                health : '.9',
+            },
+            {
+                weekday : 'Monday',
+                date : '9/18',
+                health : '.9',
+            },
+            {
+                weekday : 'Monday',
+                date : '9/19',
+                health : '.9',
+            },
+            {
+                weekday : 'Monday',
+                date : '9/20',
+                health : '.9',
+            },
+            {
+                weekday : 'Monday',
+                date : '9/21',
+                health : '.1',
+            },
+            {
+                weekday : 'Monday',
+                date : '9/22',
+                health : '.5',
+            },
+            
+        ],];
 
+        return (
+            <div className = {classes.main}>
+                <p>SELECT SCHEDULE</p>
+                {schedules.map((schedule)=>(
+                    <div>
+                        <p>{schedule[0].date}</p>
+                        <p>{schedule[6].date}</p>
+                    </div>
+                ))}
             </div>
         );
     }
