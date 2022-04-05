71a60a1b3d7a2f64230a5c424524f930bb5447b2
Trunks159 <49618679+Trunks159@users.noreply.github.com>
2022-04-05 11:48:15 -0400

    Changes to dashboard

--- sm-frontend-react/src/components/Dashboard.js
+++ sm-frontend-react/src/components/Dashboard.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import { Paper } from "@material-ui/core";
 import { withStyles } from "@material-ui/core";
-import phonySchedule from "../assets/images/Phony Schedule.svg";
+import blueLine from "../assets/images/Blue Line.svg";
+import timeLine from "../assets/images/TimeLine.svg";
 import phonyShift from "../assets/images/Phony Shift.svg";
 const styles = () => ({
   main: {
     width: "100%",
-    display : 'flex',
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  paper: {
+    maxWidth: 837,
+    height: 342,
   },
 });
 
@@ -17,13 +23,25 @@ class Dashboard extends Component {
     return (
       <div className={classes.main}>
         <div>
-          <p style = {{fontWeight : 'bold', fontSize :30, color : '#275C78' }}>Dashboard</p>
+          <p style={{ fontWeight: "bold", fontSize: 30, color: "#275C78" }}>
+            Dashboard
+          </p>
           <p>Today's Schedule</p>
-          <img src={phonySchedule} />
+          <Paper className={classes.paper}>
+            <p> Monday, 9/17</p>
+            <div style = {{display :'grid', gridTemplateColumns : '1fr 1fr', alignItems :'center'}}>
+              {'Team'}
+              <img src = {timeLine}/>
+              <p>Jordan Bless</p>
+              <img src = {blueLine} style = {{margin : 15, paddingLeft : '27%', paddingRight :'10%', }}/>
+            </div>
+            
+
+          </Paper>
         </div>
-        <div >
-            <p>My Upcoming Shifts</p>
-            <img src = {phonyShift}/>
+        <div>
+          <p>My Upcoming Shifts</p>
+          <img src={phonyShift} />
         </div>
       </div>
     );
