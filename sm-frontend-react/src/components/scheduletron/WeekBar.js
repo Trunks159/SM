de82540889ec5fb75ca66d15c87693b28ae3589e
Trunks159 <49618679+Trunks159@users.noreply.github.com>
2022-04-19 10:15:16 -0400

    some changes

--- sm-frontend-react/src/components/scheduletron/WeekBar.js
+++ sm-frontend-react/src/components/scheduletron/WeekBar.js
@@ -1,55 +1,43 @@
 import { withStyles } from "@material-ui/core";
 import React, { Component } from "react";
 import DayBtn from "./DayBtn";
+import { motion, AnimatePresence } from "framer-motion";
 
 const styles = () => ({
-  mounted: {
+  main: {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
     background: "#E1E9EE",
     justifyContent: "space-evenly",
-    width: "auto",
+    minWidth :1,
     padding: "0px 15px",
-    transition: "width 3s ease-in",
-  },
-  unmounted: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    background: "#E1E9EE",
-    justifyContent: "space-evenly",
-    width: "1px",
-    minWidth: "1px",
-    overflowX: "hidden",
-    transition: "width .5s ease-in",
   },
 });
 
 class WeekBar extends Component {
-  state = {
-    mounted: {},
-  };
-
-  componentDidMount = () => {
-    this.setState({ mounted: true });
-  };
-
   render() {
     const { week, classes } = this.props;
     return (
-      <div className={this.state.mounted ? classes.mounted : classes.unmounted}>
-        {week.map(({ weekday, staffing, month, day, id }) => (
-          <DayBtn
-            weekday={weekday}
-            completion={Math.round(
-              (staffing.actual / staffing.projected) * 100
-            )}
-            date={`${month}/${day}`}
-            id={id}
-          />
-        ))}
-      </div>
+      <AnimatePresence>
+        <motion.div
+          className={classes.main}
+          initial={{ width: '0px' }}
+          animate={{ width : '100px' }}
+          exit={{ opacity: 0 }}
+        >
+          {week.map(({ weekday, staffing, month, day, id }) => (
+            <DayBtn
+              weekday={weekday}
+              completion={Math.round(
+                (staffing.actual / staffing.projected) * 100
+              )}
+              date={`${month}/${day}`}
+              id={id}
+            />
+          ))}
+        </motion.div>
+      </AnimatePresence>
     );
   }
 }
