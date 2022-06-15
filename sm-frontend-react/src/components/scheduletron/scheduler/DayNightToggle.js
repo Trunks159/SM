eb61c98cf44b4b5a204131ed0a978ee0ee2b6012
Trunks159 <49618679+Trunks159@users.noreply.github.com>
2022-06-15 10:52:10 -0400

    changes

--- sm-frontend-react/src/components/scheduletron/scheduler/DayNightToggle.js
+++ sm-frontend-react/src/components/scheduletron/scheduler/DayNightToggle.js
@@ -8,28 +8,43 @@ import { withStyles } from "@material-ui/core";
 const styles = () => ({
   toggleGroup: {
     background: "red",
+    width: 100,
+    height: 35,
+    display: "flex",
+    justifyContent: "space-evenly",
+    marginLeft: "auto",
+  },
+  btn: {
+    flex: 1,
+    "& .MuiButtonBase-root": {
+      background: "orange",
+    },
   },
 });
 
 class DayNightToggle extends Component {
   state = { value: "day" };
-  handleValue = (e, newValue) => this.setState({ value: newValue });
+  handleValue = (e, newValue) => {
+    console.log("Newvalue: ", e.target.value);
+    this.setState({ value: newValue });
+  };
 
   render() {
     const { value } = this.state;
     const { classes } = this.props;
     return (
-      <ToggleButtonGroup value={value} onChange={this.handleValue}>
-        <ToggleButton value="day" aria-label="left aligned">
+      <div className={classes.toggleGroup}>
+        <button>
           <img style={{ opacity: value === "day" ? 1 : 0.5 }} src={dayIcon} />
-        </ToggleButton>
-        <ToggleButton value="night" aria-label="centered">
+        </button>
+
+        <button>
           <img
             style={{ opacity: value === "night" ? 1 : 0.5 }}
             src={nightIcon}
           />
-        </ToggleButton>
-      </ToggleButtonGroup>
+        </button>
+      </div>
     );
   }
 }
