9df8151d6d5b7930c1d992f1784cfa799c4386bc
Trunks159 <49618679+Trunks159@users.noreply.github.com>
2022-06-25 18:36:23 -0400

    dem changes

--- sm-frontend-react/src/components/scheduletron/mytabs/daynight/DayNightSwitches.js
+++ sm-frontend-react/src/components/scheduletron/mytabs/daynight/DayNightSwitches.js
@@ -1,31 +1,40 @@
 import * as React from "react";
 import { alpha, styled } from "@mui/material/styles";
-import { pink } from "@mui/material/colors";
 import Switch from "@mui/material/Switch";
+import dayIcon from "../../../../assets/images/Day Icon Desktop.svg";
+import nightIcon from "../../../../assets/images/Night Icon Desktop.svg";
 
 const MySwitch = styled(Switch)(({ theme }) => ({
   "& .MuiSwitch-switchBase.Mui-checked": {
+    transform: "translateX(20px)",
     color: "white",
-    "&:hover": {
-      backgroundColor: "white",
-    },
   },
   "& .MuiSwitch-track": {
+    transform: "translateX(-13px)",
     backgroundColor: "#275C78",
   },
-  width: 200,
+  "& .MuiSwitch-thumb": {
+    width : 9,
+    height : 9,
+    backgroundColor:'white'
+  },
+  width: 70,
+  margin: 0,
 }));
 
 const label = { inputProps: { "aria-label": "Switch demo" } };
 
 export default function ColorSwitches() {
   return (
-    <div style={{}}>
-      <Switch {...label} defaultChecked />
-      <Switch {...label} defaultChecked color="secondary" />
-      <Switch {...label} defaultChecked color="warning" />
-      <Switch {...label} defaultChecked color="default" />
-      <MySwitch defaultChecked />
+    <div style={{display : 'flex', marginLeft : 'auto'}}>
+      <div style={{ display: "flex", alignItems: "center", marginLeft : 'auto' }}>
+        <img style={{ marginTop: 4 }} src={dayIcon} />
+        <MySwitch />
+      </div>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img style={{ marginTop: 4 }} src={nightIcon} />
+        <MySwitch />
+      </div>
     </div>
   );
 }
