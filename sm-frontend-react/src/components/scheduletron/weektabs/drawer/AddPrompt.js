3f6c0107ea0971f2ba648c1e9e45bac93362a3a7
Trunks159 <49618679+Trunks159@users.noreply.github.com>
2022-09-27 11:14:40 -0400

    changes

--- sm-frontend-react/src/components/scheduletron/weektabs/drawer/AddPrompt.js
+++ sm-frontend-react/src/components/scheduletron/weektabs/drawer/AddPrompt.js
@@ -1,11 +1,10 @@
 import { Button, Paper } from "@mui/material";
-import React, { Component } from "react";
+import React from "react";
 import detailsIcon from "./assets/Details Icon.svg";
 import addIcon from "./assets/Add Icon.svg";
-import { withStyles } from "@material-ui/core";
-import moment from "moment";
+import { makeStyles } from "@material-ui/core";
 
-const styles = () => ({
+const userStyles = makeStyles({
   paper: {
     color: "black",
     textTransform: "capitalize",
@@ -41,64 +40,50 @@ const styles = () => ({
   },
 });
 
-class AddPrompt extends Component {
-  state = {};
-
-  addToSchedule = (scheduled, notScheduled, day, index) => {
-    const user = notScheduled.splice(index, 1)[0];
-
-    scheduled.push({
-      user: user,
-      startTime: moment(day.date).set("hour", 8),
-      endTime: moment(day.date).set("hour", 16),
-      userId: user.id,
-      dayId: day.id,
-    });
-    dispatch(updateScheduled());
-    return { scheduled: scheduled, notScheduled: notScheduled };
+const AddPrompt = ({ teamMembers, currentFunction, index, classes }) => {
+  const addToSchedule = (scheduled, notScheduled, day, userIndex) => {
+    const user = notScheduled.splice(userIndex, 1)[0];
+    dispatch(addToScheduled({ user: user, day: day }));
+    dispatch(updateNotScheduled(notScheduled));
   };
 
-  render() {
-    const { teamMembers, currentFunction, index, classes, addToSchedule } =
-      this.props;
-    return (
-      <div
-        className="add-prompt"
-        style={{
-          display: currentFunction === index ? "flex" : "none",
-          flexDirection: "column",
-        }}
-      >
-        <p style={{ fontSize: 12, color: "rgb(200, 200, 200)" }}>
-          Select team members you'd like to add to the schedule
-        </p>
+  return (
+    <div
+      className="add-prompt"
+      style={{
+        display: currentFunction === index ? "flex" : "none",
+        flexDirection: "column",
+      }}
+    >
+      <p style={{ fontSize: 12, color: "rgb(200, 200, 200)" }}>
+        Select team members you'd like to add to the schedule
+      </p>
 
-        <ul className="add-member-list">
-          {teamMembers.map((tm, i) => (
-            <li key={i}>
-              <Button
-                style={{ minWidth: 0 }}
-                onClick={() => addToSchedule(tm.id)}
-              >
-                <img src={addIcon} />
+      <ul className="add-member-list">
+        {teamMembers.map((tm, i) => (
+          <li key={i}>
+            <Button
+              style={{ minWidth: 0 }}
+              onClick={() => addToSchedule(tm.id)}
+            >
+              <img src={addIcon} />
+            </Button>
+            <Paper className={classes.paper}>
+              <p>
+                {tm.firstName}
+                <br />
+                {tm.lastName}
+              </p>
+              <i> {tm.position}</i>
+              <Button className="details">
+                <img src={detailsIcon} />
               </Button>
-              <Paper className={classes.paper}>
-                <p>
-                  {tm.firstName}
-                  <br />
-                  {tm.lastName}
-                </p>
-                <i> {tm.position}</i>
-                <Button className="details">
-                  <img src={detailsIcon} />
-                </Button>
-              </Paper>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+            </Paper>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default withStyles(styles)(AddPrompt);
+export default AddPrompt;
--- sm-frontend-react/src/reducers/scheduled.js
+++ sm-frontend-react/src/reducers/scheduled.js
@@ -1,8 +1,19 @@
+import moment from "moment";
+
 const scheduledReducer = (state = [], action) => {
   switch (action.type) {
-    case "UPDATE_SCHEDULED":
-      console.log("Redux: ", action.newScheduled);
-      return action.payLoad;
+    case "ADD_TO_SCHEDULED":
+      const { day, user } = action.payLoad;
+      return [
+        ...state,
+        {
+          user: user,
+          startTime: moment(day.date).set("hour", 8),
+          endTime: moment(day.date).set("hour", 16),
+          userId: user.id,
+          dayId: day.id,
+        },
+      ];
     case "ADD_SCHEDULED":
       return [...state, action.newScheduled];
     default:
