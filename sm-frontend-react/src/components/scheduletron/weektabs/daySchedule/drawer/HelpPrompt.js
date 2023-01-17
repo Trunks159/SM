a03480ee7e5df4752f7fbad0f23d4e4665286a11
Trunks159 <49618679+Trunks159@users.noreply.github.com>
2023-01-17 12:52:52 -0500

    changes

--- sm-frontend-react/src/components/scheduletron/weektabs/daySchedule/drawer/HelpPrompt.js
+++ sm-frontend-react/src/components/scheduletron/weektabs/daySchedule/drawer/HelpPrompt.js
@@ -2,6 +2,18 @@ import React from "react";
 import editIcon from "./assets/Edit Icon.svg";
 import dragIcon from "./assets/Drag Icon.svg";
 
+function HelpItem({ img, children }) {
+  return (
+    <div style={{ display: "flex", margin: 20 }}>
+      <div style={{ width: "30%" }}>{img}</div>
+      <div style={{ flex: 1 }}>
+        <h2>{children.header}</h2>
+        <p>{children.content}</p>
+      </div>
+    </div>
+  );
+}
+
 function HelpPrompt({ currentFunction, name }) {
   return (
     <div
@@ -13,34 +25,34 @@ function HelpPrompt({ currentFunction, name }) {
       <h3>Let me help you out a bit</h3>
 
       <h6>To adjust the time each team member is scheduled for:</h6>
-      <div style={{ display: "flex" , background : 'red',}}>
-        <img
-          alt="Drag"
-          src={dragIcon}
-          style={{
-            width: 15,
-            background: "rgba(0,0,0,.56)",
-            alignSelf: "start",
-            marginTop: 30,
-            padding: "8px 12px",
-            borderRadius: 3,
-          }}
-        />
-        <div>
-          <h2>Drag/Spread</h2>
-          <p>
-            Drag the spread indicators to stretch or expand each team member's
-            work time.
-          </p>
-        </div>
-      </div>
-      <div>
-        <img alt="Edit" src={editIcon} />
-        <div>
-          <h2>Select</h2>
-          <p>Select a team member's time to manually enter their time.</p>
-        </div>
-      </div>
+      <HelpItem
+        img={
+          <img
+            alt="Drag"
+            src={dragIcon}
+            style={{
+              width: 15,
+              background: "rgba(0,0,0,.56)",
+              alignSelf: "start",
+              padding: "8px 12px",
+              borderRadius: 3,
+              marginTop: 50,
+            }}
+          />
+        }
+      >
+        {{
+          header: "Drag/Spread",
+          content:
+            "Drag the spread indicators to stretch or expand each team member's work time.",
+        }}
+      </HelpItem>
+      <HelpItem img={<img alt="Edit" src={editIcon} />}>
+        {{
+          header: "Select",
+          content: "Select a team member's time to manually enter their time.",
+        }}
+      </HelpItem>
     </div>
   );
 }
