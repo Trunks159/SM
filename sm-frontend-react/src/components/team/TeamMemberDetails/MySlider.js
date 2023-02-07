072e00e14b38a23c758c400bc7c7aae21b5a2261
Trunks159 <49618679+Trunks159@users.noreply.github.com>
2023-02-07 11:49:14 -0500

    Working on some time functions

--- sm-frontend-react/src/components/team/TeamMemberDetails/MySlider.js
+++ sm-frontend-react/src/components/team/TeamMemberDetails/MySlider.js
@@ -3,31 +3,47 @@ import { Slider } from "@mui/material";
 import React, { useState } from "react";
 import moment from "moment";
 
+const BASE_DATE = "1970-01-01 00";
+/*
+So we need perc to time and time to perc
+if times come in convert them to perc
+if false, turn slider off and set values to 0,100
+if true , max and min are set to 0 and 100
+*/
+
+function timeToSliderValue(time) {
+  //take time and convert to a number between 0 and 100
+  //take time that comes in and make a moment , set it to 12AM
+  //add a day to that
+  //find difference in all of that
+  //compare that to the difference between input time and such
+  const momentTime = moment(time);
+  const start = momentTime.hour(0).minute(0);
+  const end = start.add(1, "days");
+  return (
+    (momentTime.diff(start, "hours", true) / end.diff(start, "hours", true)) *
+    100
+  );
+}
+
+function sliderValueToTime(sliderValue) {}
+
 const StyledSlider = styled(Slider)({});
-//make a date and use it 
+//make a date and use it
 
-function makeFullAvailability(){
-    const startTime = moment().hour(0).minute(0).second(0).millisecond(0);
-const endTime = startTime.add(1, 'days');
-return [startTime.format(), endTime.format()];
+function setDefaultSliderValues(availability) {
+  if (availability === true) {
+    //make moments and set times to
+    return [0, 100];
+  } else if (Array.isArray(availability)) {
+    return availability.map((time) => timeToSliderValue(time));
+  }
+  return [0, 50];
 }
 
-function doStuff(availability){
-    //if availability === true that means its open availability
-    //if its an array use the 2 values to set the sliders
-    //if false turn the slider off because they are not available
-    if(availability){
-        return (availability === true ? (1) : 2)
-    }
-    return false
-}
-
-function MySlider({defaultAvailability}) {
-
-    [availability, setAvailability] = useState(defaultAvailability === true ? makeFullAvailability() : []);
-    
-    console.log('AV: ', av)
-  return <StyledSlider value={[startTime, endTime]} />;
+function MySlider({ defaultAvailability }) {
+  const [availability, setAvailability] = useState([]);
+  return <StyledSlider />;
 }
 
 export default MySlider;
